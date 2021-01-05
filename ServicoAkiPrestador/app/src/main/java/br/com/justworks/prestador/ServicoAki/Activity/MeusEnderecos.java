c08bd4c515481aaa310376926fb05663af44bed7
guiganafe@outlook.com <14*71999>
2021-01-05 15:05:25 -0400

    Inicio tela Meus Endereços

---
+++ ServicoAkiPrestador/app/src/main/java/br/com/justworks/prestador/ServicoAki/Activity/MeusEnderecos.java
@@ -0,0 +1,16 @@
+package br.com.justworks.prestador.ServicoAki.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.justworks.prestador.ServicoAki.R;
+
+public class MeusEnderecos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meus_enderecos);
+    }
+}
